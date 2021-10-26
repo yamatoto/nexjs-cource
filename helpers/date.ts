38100cb57f617aabb4c1aaca6faa4b6e232f1683
yamatoto <mftb35ymt@yahoo.co.jp>
2021-10-27 02:14:52 +0900

    date helperで YMD と YM 分ける

--- helpers/date.ts
+++ helpers/date.ts
@@ -1,7 +1,14 @@
-export function toLocalDate(dateStr: string): string {
+export function toLocalYMD(dateStr: string): string {
   return new Date(dateStr).toLocaleDateString("ja-JP", {
     year: "numeric",
     month: "long",
     day: "numeric",
+  });
+}
+
+export function toLocalYM(date: Date): string {
+  return date.toLocaleDateString("ja-JP", {
+    year: "numeric",
+    month: "long",
   });
 }
