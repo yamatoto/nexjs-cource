acad13d15fce8ea7931b058cd9ba25dbcb1b408e
yamatoto <mftb35ymt@yahoo.co.jp>
2021-11-07 12:47:46 +0900

    AllPostsPage実装

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,9 +1,46 @@
+import AllPosts from "../../components/posts/all-posts";
+import { Post } from "../../model/post";
+
+const DUMMY_POSTS: Post[] = [
+  {
+    _id: "p1",
+    slug: "getting-started-with-nextjs",
+    title: "Getting Started with NextJS",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "NextJS is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.",
+    date: "2022-02-10",
+  },
+  {
+    _id: "p2",
+    slug: "getting-started-with-nextjs2",
+    title: "Getting Started with NextJS",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "NextJS is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.",
+    date: "2022-02-10",
+  },
+  {
+    _id: "p3",
+    slug: "getting-started-with-nextjs3",
+    title: "Getting Started with NextJS",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "NextJS is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.",
+    date: "2022-02-10",
+  },
+  {
+    _id: "p4",
+    slug: "nextjs-file-based-routing",
+    title: "Getting Started with NextJS",
+    image: "nextjs-file-based-routing.png",
+    excerpt:
+      "NextJS is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.",
+    date: "2022-02-10",
+  },
+];
 function AllPostsPage() {
-  return (
-    <div>
-      <h1>AllPosts</h1>
-    </div>
-  );
+  return <AllPosts posts={DUMMY_POSTS} />;
 }
 
 export default AllPostsPage;
