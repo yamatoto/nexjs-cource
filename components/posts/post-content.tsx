14d154c8c52c0c95d8560ff449702419dc9aec07
yamatoto <mftb35ymt@yahoo.co.jp>
2021-11-08 02:24:59 +0900

    mdファイルからImageタグ使う

--- components/posts/post-content.tsx
+++ components/posts/post-content.tsx
@@ -2,14 +2,39 @@ import classes from "./post-content.module.css";
 import { Post } from "../../model/post";
 import PostHeader from "./post-header";
 import ReactMarkdown from "react-markdown";
+import { SpecialComponents } from "react-markdown/lib/ast-to-react";
+import { NormalComponents } from "react-markdown/lib/complex-types";
+import Image from "next/image";
+import React from "react";
 
 function PostContent({ title, slug, image, content }: Post) {
   const imagePath = `/images/posts/${slug}/${image}`;
-
+  const customRenderers: Partial<
+    Omit<NormalComponents, keyof SpecialComponents> & SpecialComponents
+  > = {
+    p({ node, children }) {
+      const child = node.children[0];
+      if (child.type === "element" && child.tagName === "img") {
+        const { properties } = child;
+        const { src, alt } = properties!;
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${slug}/${src}`}
+              alt={String(alt)}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+      return <p>{children}</p>;
+    },
+  };
   return (
     <article className={classes.content}>
       <PostHeader title={title} imagePath={imagePath} />
-      <ReactMarkdown>{content}</ReactMarkdown>
+      <ReactMarkdown components={customRenderers}>{content}</ReactMarkdown>
     </article>
   );
 }
